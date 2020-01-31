b61499b3b402aa1b670dab086eb9a5fcf7f5a9fb
Richie Bendall <richiebendall@gmail.com>
2020-01-31 23:31:28 +1300

    Initial commit

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+/**
+ * My awesome module.
+ * @param input Lorem ipsum.
+ * @param postfix Lorem ipsum.
+ * @example
+ * ```
+ * const theModule = require("the-module");
+ * theModule("unicorns");
+ * //=> 'unicorns & rainbows'
+ * ```
+*/
+declare function theModule(input: string, { postfix }: { postfix?: string }): string;
+
+export = theModule;
---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict"
+
+module.exports = (input, { postfix = "rainbows" } = {}) => {
+	if (typeof input !== "string") throw new TypeError(`Expected a string, got ${typeof input}`)
+
+	return `${input} & ${postfix}`
+}
