3a239aec0ed9afa6cf960d8ae3d21878cd6bb57a
Richie Bendall <richiebendall@gmail.com>
2020-02-23 04:59:43 +1300

    chore: Removed duplication
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ module.exports = () => {
 	const comboMarksSupplementRange = "\\u1dc0-\\u1dff"
 	const comboRange = comboMarksRange + comboHalfMarksRange + comboSymbolsRange + comboMarksExtendedRange + comboMarksSupplementRange
 	const varRange = "\\ufe0e\\ufe0f"
-	const familyRange = "\\uD83D\\uDC69\\uD83C\\uDFFB\\u200D\\uD83C\\uDF93"
 
 	// Used to compose unicode capture groups.
 	const astral = `[${astralRange}]`
@@ -20,11 +19,10 @@ module.exports = () => {
 	const fitz = "\\ud83c[\\udffb-\\udfff]"
 	const modifier = `(?:${combo}|${fitz})`
 	const nonAstral = `[^${astralRange}]`
-	const regional = "(?:\\uD83C[\\uDDE6-\\uDDFF]){2}"
+	const regional = "(?:\\ud83c[\\udde6-\\uddff]){2}"
 	const surrogatePair = "[\\ud800-\\udbff][\\udc00-\\udfff]"
 	const zwj = "\\u200d"
 	const blackFlag = "(?:\\ud83c\\udff4\\udb40\\udc67\\udb40\\udc62\\udb40(?:\\udc65|\\udc73|\\udc77)\\udb40(?:\\udc6e|\\udc63|\\udc6c)\\udb40(?:\\udc67|\\udc74|\\udc73)\\udb40\\udc7f)"
-	const family = `[${familyRange}]`
 
 	// Used to compose unicode regexes.
 	const optModifier = `${modifier}?`
@@ -32,8 +30,8 @@ module.exports = () => {
 	const optJoin = `(?:${zwj}(?:${[nonAstral, regional, surrogatePair].join("|")})${optVar + optModifier})*`
 	const seq = optVar + optModifier + optJoin
 	const nonAstralCombo = `${nonAstral}${combo}?`
-	const symbol = `(?:${[blackFlag, nonAstralCombo, combo, regional, surrogatePair, astral, family].join("|")})`
+	const symbol = `(?:${[blackFlag, nonAstralCombo, combo, regional, surrogatePair, astral].join("|")})`
 
-	// Used to match [String symbols](https://mathiasbynens.be/notes/javascript-unicode).
+	// Used to match [string symbols](https://mathiasbynens.be/notes/javascript-unicode).
 	return new RegExp(`${fitz}(?=${fitz})|${symbol + seq}`, "g")
 }
