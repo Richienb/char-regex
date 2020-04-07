aee88db417e6d137aa7da3f7140222e75b9599fc
Richie Bendall <richiebendall@gmail.com>
2020-04-07 16:09:00 +1200

    Update meta
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- test.js
+++ test.js
@@ -3,14 +3,14 @@ const allChars = require("all-chars")()
 const charRegex = require(".")()
 
 // See: https://mathiasbynens.be/notes/javascript-unicode#poo-test
-test("The Pile of Poo Test™", (t) => {
+test("The Pile of Poo Test™", t => {
 	t.deepEqual("Iñtërnâtiônàlizætiøn☃💩".match(charRegex), [
-		"I", "ñ", "t", "ë", "r", "n", "â", "t", "i", "ô", "n", "à", "l", "i", "z", "æ", "t", "i", "ø", "n", "☃", "💩",
+		"I", "ñ", "t", "ë", "r", "n", "â", "t", "i", "ô", "n", "à", "l", "i", "z", "æ", "t", "i", "ø", "n", "☃", "💩"
 	])
 })
 
-allChars.forEach((character) => {
-	test(`Test "${character}"`, (t) => {
+allChars.forEach(character => {
+	test(`Test "${character}"`, t => {
 		t.deepEqual(character.match(charRegex), [character])
 	})
 })
